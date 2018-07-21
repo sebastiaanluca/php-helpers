6eafecb6a5451447e47da35d3cb80c401e806a42
Sebastiaan Luca <hello@sebastiaanluca.com>
2018-07-21 23:02:08 +0200

    Add constants helper trait

---
+++ src/Classes/Constants.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace SebastiaanLuca\PhpHelpers\Classes;
+
+use ReflectionClass;
+
+trait Constants
+{
+    /**
+     * Get all the class constants.
+     *
+     * @return array
+     */
+    public static function constants() : array
+    {
+        return (new ReflectionClass(__CLASS__))->getConstants();
+    }
+
+    /**
+     * Get all the names of the class constants.
+     *
+     * @return array
+     */
+    public static function keys() : array
+    {
+        return array_keys(static::all());
+    }
+
+    /**
+     * Get all the values of the class constants.
+     *
+     * @return array
+     */
+    public static function values() : array
+    {
+        return array_values(static::all());
+    }
+}
---
+++ tests/Unit/Classes/ConstantsHelperTest.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace SebastiaanLuca\Helpers\Tests\Unit\Classes;
+
+use SebastiaanLuca\PhpHelpers\Classes\Constants;
+use SebastiaanLuca\PhpHelpers\Tests\TestCase;
+
+class ConstantsHelperTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function it returns all constants() : void
+    {
+        $class = new class
+        {
+            use Constants;
+
+            public const FIRST_CONSTANT = 1;
+            public const SECOND_CONSTANT = 2;
+            public const THIRD_CONSTANT = 3;
+        };
+
+        $this->assertEquals([
+            'FIRST_CONSTANT' => 1,
+            'SECOND_CONSTANT' => 2,
+            'THIRD_CONSTANT' => 3,
+        ], $class::constants());
+    }
+
+    /**
+     * @test
+     */
+    public function it returns all constant names() : void
+    {
+        $class = new class
+        {
+            use Constants;
+
+            public const FIRST_CONSTANT = 1;
+            public const SECOND_CONSTANT = 2;
+            public const THIRD_CONSTANT = 'three';
+        };
+
+        $this->assertEquals([
+            'FIRST_CONSTANT',
+            'SECOND_CONSTANT',
+            'THIRD_CONSTANT',
+        ], $class::keys());
+    }
+
+    /**
+     * @test
+     */
+    public function it returns all constant values() : void
+    {
+        $class = new class
+        {
+            use Constants;
+
+            public const FIRST_CONSTANT = 1;
+            public const SECOND_CONSTANT = 'two';
+            public const THIRD_CONSTANT = 3;
+        };
+
+        $this->assertEquals([
+            1,
+            'two',
+            3,
+        ], $class::values());
+    }
+}
