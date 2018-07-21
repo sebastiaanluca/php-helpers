b885d096d6c4a53001a19b6165dfd2dbe1ce64ee
Sebastiaan Luca <hello@sebastiaanluca.com>
2018-07-21 23:02:26 +0200

    Add method helper

---
+++ src/Classes/MethodHelper.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace SebastiaanLuca\PhpHelpers\Classes;
+
+use Error;
+use ReflectionException;
+use ReflectionMethod;
+
+class MethodHelper
+{
+    /**
+     * Check if an object has a given method of a given type.
+     *
+     * @param object $object
+     * @param string $method
+     * @param string $type
+     *
+     * @return bool
+     */
+    public static function hasMethodOfType($object, $method, $type) : bool
+    {
+        if (! method_exists($object, $method)) {
+            return false;
+        }
+
+        try {
+            $reflection = new ReflectionMethod($object, $method);
+            // FIXME
+            $type = 'is' . studly_case($type);
+
+            return $reflection->{$type}();
+        } catch (ReflectionException $exception) {
+            return false;
+        } catch (Error $exception) {
+            return false;
+        }
+    }
+
+    /**
+     * Check if an object has a given protected method.
+     *
+     * @param object $object
+     * @param string $method
+     *
+     * @return bool
+     */
+    public static function hasProtectedMethod($object, $method) : bool
+    {
+        return static::hasMethodOfType($object, $method, 'protected');
+    }
+
+    /**
+     * Check if an object has a given public method.
+     *
+     * @param object $object
+     * @param string $method
+     *
+     * @return bool
+     */
+    public static function hasPublicMethod($object, $method) : bool
+    {
+        return static::hasMethodOfType($object, $method, 'public');
+    }
+}
---
+++ tests/Unit/Classes/MethodHelperTest.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace SebastiaanLuca\Helpers\Tests\Unit\Classes;
+
+use SebastiaanLuca\PhpHelpers\Classes\MethodHelper;
+use SebastiaanLuca\PhpHelpers\Tests\TestCase;
+
+class MethodHelperTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function it detects if a method of a certain visibility exists() : void
+    {
+        $class = new class
+        {
+            private function myMethod() : bool
+            {
+                return true;
+            }
+        };
+
+        $this->assertTrue(MethodHelper::hasMethodOfType($class, 'myMethod', 'private'));
+    }
+
+    /**
+     * @test
+     */
+    public function it detects a missing method() : void
+    {
+        $class = new class
+        {
+            protected function myMethod() : bool
+            {
+                return true;
+            }
+        };
+
+        $this->assertFalse(MethodHelper::hasMethodOfType($class, 'myInvalidMethod', 'protected'));
+    }
+
+    /**
+     * @test
+     */
+    public function it detects a method with different visibility() : void
+    {
+        $class = new class
+        {
+            public function myMethod() : bool
+            {
+                return true;
+            }
+        };
+
+        $this->assertFalse(MethodHelper::hasMethodOfType($class, 'myMethod', 'private'));
+    }
+
+    /**
+     * @test
+     */
+    public function it detects a protected method() : void
+    {
+        $class = new class
+        {
+            protected function myMethod() : bool
+            {
+                return true;
+            }
+        };
+
+        $this->assertTrue(MethodHelper::hasProtectedMethod($class, 'myMethod'));
+    }
+
+    /**
+     * @test
+     */
+    public function it detects a public method() : void
+    {
+        $class = new class
+        {
+            public function myMethod() : bool
+            {
+                return true;
+            }
+        };
+
+        $this->assertTrue(MethodHelper::hasPublicMethod($class, 'myMethod'));
+    }
+}
