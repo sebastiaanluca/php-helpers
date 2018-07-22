9fc81aba6205ed890bc09c24eae024820b48bc94
Sebastiaan Luca <hello@sebastiaanluca.com>
2018-07-22 15:31:44 +0200

    Fix array helpers
    Without using any other packages.

--- src/Functions/generic.php
+++ src/Functions/generic.php
@@ -2,6 +2,7 @@
 
 use Carbon\Carbon;
 use SebastiaanLuca\PhpHelpers\Classes\MethodHelper;
+use SebastiaanLuca\PhpHelpers\InternalHelpers;
 
 if (! function_exists('rand_bool')) {
     /**
@@ -64,8 +65,7 @@ if (! function_exists('array_expand')) {
         $expanded = [];
 
         foreach ($array as $key => $value) {
-            // FIXME
-            array_set($expanded, $key, $value);
+            InternalHelpers::arraySet($expanded, $key, $value);
         }
 
         return $expanded;
@@ -83,8 +83,9 @@ if (! function_exists('array_without')) {
      */
     function array_without(array $array, $values) : array
     {
-        // FIXME
-        return array_values(array_diff($array, array_wrap($values)));
+        $values = ! is_array($values) ? [$values] : $values;
+
+        return array_values(array_diff($array, $values));
     }
 }
 
